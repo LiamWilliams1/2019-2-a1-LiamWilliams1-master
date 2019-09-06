c10e4be1c183d6ca1d83ba0e98f4879d5d21a6bb
LiamWilliams1 <liam.williams@my.jcu.edu.au>
2019-09-06 11:01:51 +1000

    add display list function add FILE_NAME as a variable for CSV file

--- assignment1.py
+++ assignment1.py
@@ -4,48 +4,62 @@ Name: Liam Williams
 Date started: 27/08/19
 GitHub URL: https://github.com/cp1404-students/2019-2-a1-LiamWilliams1
 """
-
+FILE_NAME = "movies.csv"
 MENU_OPTIONS = ["Q", "L", "A", "W"]
-MENU = """MENU:
-L - List Movies
+MENU = """L - List Movies
 A - Add new movie
 W - watch a movie
 Q - Quit \n >>> """
 
-def load_movies():
+
+
+def load_movie():
     lists_of_movies = []
+    count_movies = 0
     in_file = 0
     try:
-        in_file = open("movies.csv", 'r')
+        in_file = open(FILE_NAME, 'r')
     except FileNotFoundError:
         print("File not found")
     for movie in in_file:
         movie = movie.strip("\n")
         new_list = movie.split(",")
         lists_of_movies.append(new_list)
+        count_movies += 1
     in_file.close()
-    print(in_file)
-    return lists_of_movies
+    return lists_of_movies, count_movies
+
+
+def display_list(movie_lists):
+    tag = ()
+    count = 0
+    index = 0
+    for movie in movie_lists:
+
+        if movie[3] == "w":
+            tag = "*"
+            count += 1
+        else:
+            tag = " "
+
+        print("{:3} {:2} {:35} {:25} ({})".format(index, tag, movie[0], movie[1], movie[2]))
+        index = index + 1
+    print(count, "movies watch", len(movie_lists) - count, "movies left to watch")
+
 
 
 def main():
-
-    """..."""
-    print("Movies To Watch 1.0 - by <Liam Williams>")
-    #print({} .format(movies_loaded_count))
-    lists_of_movies = []
+    print("Songs To Learn 1.0 - by <Liam Williams>")
+    lists_of_movies, counter = load_movie()
+    print(counter, "movies loaded")
     Menu_Choice = input(MENU).upper().strip()
     while Menu_Choice != "Q":
         if Menu_Choice == "L":
-            print("you sele ted l")
-        elif Menu_Choice == "A":
-            print("you select a")
-        elif Menu_Choice == "W":
-
-
+            display_list(lists_of_movies)
+        else:
             print("invalid Input please try again")
         Menu_Choice = input(MENU).upper()
-    print("end")
+    print(len(lists_of_movies), "movies saved to movies.CVS\nhave a nice day :)")
 
 
 if __name__ == '__main__':
