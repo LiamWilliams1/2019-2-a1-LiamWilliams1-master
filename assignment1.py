f8664a3a0fb2338c0e2b5a688682358c8619209c
LiamWilliams1 <liam.williams@my.jcu.edu.au>
2019-09-07 17:07:29 +1000

    add add_movie function format add movie print

--- assignment1.py
+++ assignment1.py
@@ -5,7 +5,7 @@ Date started: 27/08/19
 GitHub URL: https://github.com/cp1404-students/2019-2-a1-LiamWilliams1
 """
 FILE_NAME = "movies.csv"
-MENU_OPTIONS = ["Q", "L", "A", "W"]
+MENU_OPTIONS = ["Q", "L", "A", "W" "C"]
 MENU = """L - List Movies
 A - Add new movie
 W - watch a movie
@@ -42,11 +42,30 @@ def display_list(movie_lists):
         else:
             tag = " "
 
-        print("{:3} {:2} {:35} {:25} ({})".format(index, tag, movie[0], movie[1], movie[2]))
+        print("{:3} {:2} {:} {:} ({})".format(index, tag, movie[0], movie[1], movie[2]))
         index = index + 1
     print(count, "movies watch", len(movie_lists) - count, "movies left to watch")
 
+#my intial attempt at an add movie function. For some reason I wouldn't append to the movie list. I couldn't figure out why.
+#def add_movie_check(lists_of_movies):
+ #   movie_name = input("Title:")
+  #  while len(movie_name) < 1:
+   #     print("input cannot be blank")
+    #    movie_name = input("Title:")
+    #year = input("year")
+    #while len(year) < 1:
+     #   print("input cannot be blank")
+     #   year = input("year")
+    #Category = input("input genre")
+    #while len(Category) < 1:
+     #   print("input cannot be blank")
+     #   genre = input("input genre")
+    #watched = ("u")
+    #lists_of_movies.append([movie_name, Category, year, watched])
+    #print("{}({} from {}) added to movie list".format(movie_name, Category, year,))
+
 def add_movie(lists_of_movies):
+    #new_movie = [lists_of_movies]
     movie_name = input("Title:")
     while len(movie_name) < 1:
         print("input cannot be blank")
@@ -60,8 +79,12 @@ def add_movie(lists_of_movies):
         print("input cannot be blank")
         genre = input("input genre")
     watched = ("u")
-    lists_of_movies.append([movie_name, Category, year, ])
-    print("{}({} from {}) added to movie list".format(movie_name, Category, year,))
+    lists_of_movies.append([movie_name, Category, year, watched])
+    #new_movie.append([movie_name, Category, year, watched])
+    #print(new_movie)
+    #print(lists_of_movies)
+    print("{}({}from {}) added to movie list".format(movie_name, Category, year,))
+
 
 #def quit():
 
@@ -70,11 +93,14 @@ def main():
     lists_of_movies, counter = load_movie()
     print(counter, "movies loaded")
     Menu_Choice = input(MENU).upper().strip()
-    if Menu_Choice == "Q":
+    while Menu_Choice != "Q":
         if Menu_Choice == "L":
             display_list(lists_of_movies)
         elif Menu_Choice == "A":
             add_movie(lists_of_movies)
+        elif Menu_Choice == "W":
+            #watchmovie(lists_of_movies)
+
         else:
             print("invalid Input please try again")
         Menu_Choice = input(MENU).upper()
